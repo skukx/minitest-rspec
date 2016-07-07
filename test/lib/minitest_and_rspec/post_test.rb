d624ef000f9d139fd4a8818cbc80baee821fa9d0
Taylor Scott <tscott@deseretbook.com>
2016-07-07 12:21:34 -0600

    wrote minitests

--- test/lib/minitest_and_rspec/post_test.rb
+++ test/lib/minitest_and_rspec/post_test.rb
@@ -1,4 +1,32 @@
 require 'test_helper'
 
 class PostTest < Minitest::Test
+  def setup
+    @subject = MinitestAndRspec::Post.new name: 'John Doe', message: 'Hello World!'
+  end
+
+  def test_respond_to
+    assert_respond_to(@subject, :name)
+    assert_respond_to(@subject, :message)
+    assert_respond_to(@subject, :votes)
+  end
+
+  def test_values
+    assert_equal('John Doe', @subject.name)
+    assert_equal('Hello World!', @subject.message)
+    assert_equal(0, @subject.votes)
+  end
+
+  def test_up_votes
+    @subject.up_vote
+    @subject.up_vote
+
+    assert_equal(2, @subject.votes)
+  end
+
+  def test_down_votes
+    @subject.down_vote
+
+    assert_equal(-1, @subject.votes)
+  end
 end
